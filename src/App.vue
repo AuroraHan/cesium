8eb49e13758937bdae4ceb034572f3f2923987c6
AuroraHan <1224195854@qq.com>
2023-04-02 00:53:10 +0800

    初始华仓库

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium';
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer');
+})
+
+
+</script>
+
+<style scoped></style>
