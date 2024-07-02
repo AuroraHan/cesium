3d60e2456d39f7c159676c7cd21cec68fabf85f2
AuroraHan <1224195854@qq.com>
2024-07-02 22:20:10 +0800

    精简代码

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -13,14 +13,6 @@ const { viewer } = store.state
 
 onMounted(() => {
     console.log(viewer,'1111');
-    // const viewer = new Cesium.Viewer('cesiumContainer', {
-    //     infoBox: false,
-    //     //设置地形
-    //     // terrainProvider: Cesium.createWorldTerrain({
-    //     //     requestWaterMask: true,
-    //     //     requestVertexNormals: true
-    //     // })
-    // });
 
     // 设置地球的初始位置
     const initialPosition = Cesium.Cartesian3.fromDegrees(112.199265, 31.035423,10000000); // 荆门的经纬度，高度为10,000,000米
@@ -41,27 +33,8 @@ onMounted(() => {
         credit: 'Amap'
     })
     viewer.imageryLayers.addImageryProvider(gaodeImageryProvider)
-
-    //加载地形数据
-    // const terrainProvider = new Cesium.CesiumTerrainProvider({
-    //     url: Cesium.IonResource.fromAssetId(1),
-    //     requestWaterMask: true,//请求水体效果所需要的海岸线数据
-    //     requestVertexNormals: true,//请求地形照明数据
-    // })
-    // viewer.terrainProvider = terrainProvider
 })
 
-const addEarch =async ()=>{
-    const initialPosition = Cesium.Cartesian3.fromDegrees(112.199265, 31.035423, 6000000); // 荆门的经纬度，高度为10,000,000米
-    viewer.scene.camera.setView({
-        destination: initialPosition,
-        // orientation: {
-        //     heading: Cesium.Math.toRadians(0),     // 指南针方向（北方向为0度）
-        //     pitch: Cesium.Math.toRadians(20),    // 俯仰角
-        //     roll: 0                                // 横滚角
-        // }
-    });
-}
 
 
 </script>
